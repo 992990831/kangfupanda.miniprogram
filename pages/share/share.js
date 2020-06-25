64bc109b06ccfb59c6a67f54e97b6d6b9f80914a
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-25 13:08:07 +0800

    自定义转发微信好友按钮

---
+++ pages/share/share.js
@@ -0,0 +1,80 @@
+// pages/share/share.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+      this.setData({
+        poster: options.poster,
+        title: options.title,
+        search: options.search
+      })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+    return {
+      title: this.data.title,
+      path: `/pages/index/index?url=${this.data.search}`,
+      imageUrl: `https://api.kangfupanda.com/Upload/${this.data.poster}`,
+      success: (res) => {
+        console.log("转发成功", res);
+      },
+      fail: (res) => {
+        console.log("转发失败", res);
+      }
+    }
+  }
+})
